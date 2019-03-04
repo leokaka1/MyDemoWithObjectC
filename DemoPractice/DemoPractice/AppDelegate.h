3d7685a0743007b55ac3dbdcce77ac09fdccfb39
leokaka1 <leocaoxiaozhu@163.com>
2019-03-04 09:04:44 +0800

    first update

---
+++ DemoPractice/DemoPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoPractice
+//
+//  Created by Leon on 2019/3/4.
+//  Copyright © 2019 Leon 笑 竹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoPractice/DemoPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoPractice
+//
+//  Created by Leon on 2019/3/4.
+//  Copyright © 2019 Leon 笑 竹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
